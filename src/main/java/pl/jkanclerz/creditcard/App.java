9b1949672428db89704e3ec5722a4a7720b0f89f
s-014-17 <s-014-17@gumiforest.ae.krakow.pl>
2022-03-21 11:52:42 +0100

    inital commit for ecommerce project

---
+++ src/main/java/pl/jkanclerz/creditcard/App.java
@@ -0,0 +1,8 @@
+package pl.jkanclerz.creditcard;
+
+import java.math.BigDecimal;
+
+public class App {
+    public static void main(String[] args) {
+    }
+}
---
+++ src/main/java/pl/jkanclerz/creditcard/CantAssignLimtiTwiceException.java
@@ -0,0 +1,4 @@
+package pl.jkanclerz.creditcard;
+
+public class CantAssignLimtiTwiceException extends IllegalStateException {
+}

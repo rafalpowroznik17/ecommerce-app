eb84877c378ad032539dbeedb89c9f97a92a0822
s-014-17 <s-014-17@gumiforest.ae.krakow.pl>
2022-03-28 12:52:06 +0200

    expose product catalog via http

---
+++ src/main/java/pl/jkanclerz/App.java
@@ -0,0 +1,38 @@
+package pl.jkanclerz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import pl.jkanclerz.creditcard.NameProvider;
+import pl.jkanclerz.productcatalog.ProductCatalog;
+
+import java.math.BigDecimal;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+
+    }
+
+    @Bean
+    NameProvider createNameProvider() {
+        return new NameProvider();
+    }
+
+    @Bean
+    ProductCatalog createMyProductCatalog() {
+        ProductCatalog productCatalog = new ProductCatalog();
+        String productId1 = productCatalog.addProduct("lego-set-1", "Nice Lego set");
+        productCatalog.assignImage(productId1, "https://picsum.photos/id/237/200/300");
+        productCatalog.assignPrice(productId1, BigDecimal.TEN);
+        productCatalog.publish(productId1);
+
+        String productId2 = productCatalog.addProduct("lego-set-2", "Even nicer Lego set");
+        productCatalog.assignImage(productId2, "https://picsum.photos/id/238/200/300");
+        productCatalog.assignPrice(productId2, BigDecimal.valueOf(20.20));
+        productCatalog.publish(productId2);
+
+        return productCatalog;
+    }
+}
